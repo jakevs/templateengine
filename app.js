d618a3091aea18bdfdd3f1087f3c87d3ecaad394
James Van Slyke <jakevs3@gmail.com>
2020-05-16 15:52:26 -0500

    getting node terminal to work

--- app.js
+++ app.js
@@ -1,118 +1,229 @@
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const inquirer = require("inquirer");
+// const path = require("path");
+// const fs = require("fs");
+
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// const render = require("./lib/htmlRenderer");
+// const teamMembers = [];
+// const idArray = [];
+
+// function menu() {
+//   function createManager() {
+//     console.log("Please Build Your Team!");
+//     inquirer
+//       .prompt([
+//         {
+//           name: "managerName",
+//           message: "Please enter manager's name: ",
+//           validate: validateName,
+//         },
+//         {
+//           name: "managerId",
+//           message: "Please enter manager's id: ",
+//           validate: validateId,
+//         },
+//         {
+//           name: "managerEmail",
+//           message: "Please enter manager's email: ",
+//           default: "mail@mail.com",
+//           validate: validateEmail,
+//         },
+//         {
+//           type: "list",
+//           name: "employeeType",
+//           message: "Who would you like to enter first?",
+//           choices: ["Manager", "Engineer", "Intern"],
+//         },
+//         {
+//           name: "managerOfficeNumber",
+//           message: "Please enter office phone number",
+//           when: function (answers) {
+//             return answers["employeeType"] === "Manager";
+//           },
+//         },
+
+//         {
+//           name: "gitHub",
+//           message: "Please enter gitHub account id",
+//           when: function (answers) {
+//             return answers["employeeType"] === "Engineer";
+//           },
+//         },
+//         {
+//           name: "school",
+//           message: "Please enter school",
+//           when: function (answers) {
+//             return answers["employeeType"] === "Intern";
+//           },
+//         },
+//         {
+//           type: "confirm",
+//           name: "again",
+//           message: "Would you like to enter more employees?",
+//         },
+//       ])
+//       .then((answers) => {
+//         const manager = new Manager(
+//           answers.managerName,
+//           answers.managerId,
+//           answers.managerEmail,
+//           answers.managerOfficeNumber
+//         );
+
+//         teamMembers.push(manager);
+//         idArray.push(answers.managerId);
+
+//         //createTeam() - this function is going to ask who to create next and then either create intern or engineer
+//       });
+//     createManager();
+//   }
+
+//   function createTeam() {
+//     inquirer
+//       .prompt([
+//         {}, //ask who would you like to create with choices engineer intern or none
+//       ])
+//       .then((choice) => {
+//         switch (answers.employeeType) {
+//           case value:
+//             break;
+
+//           default:
+//             break;
+//         }
+//       });
+//   }
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const eeQuestions = async (inputs = []) => {
+  const prompts = [
+    {
+      name: "employeeName",
+      message: "Please enter employee name: ",
+      validate: validateName,
+    },
+    {
+      name: "employeeId",
+      message: "Please enter employee id: ",
+      validate: validateId,
+    },
+    {
+      name: "employeeEmail",
+      message: "Please enter employee email: ",
+      default: "mail@mail.com",
+      validate: validateEmail,
+    },
+    {
+      type: "list",
+      name: "employeeType",
+      message: "Who would you like to enter first?",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      name: "officeNumber",
+      message: "Please enter office phone number",
+      when: function (answers) {
+        return answers["employeeType"] === "Manager";
+      },
+    },
+    {
+      name: "gitHub",
+      message: "Please enter gitHub account id",
+      when: function (answers) {
+        return answers["employeeType"] === "Engineer";
+      },
+    },
+    {
+      name: "school",
+      message: "Please enter school",
+      when: function (answers) {
+        return answers["employeeType"] === "Intern";
+      },
+    },
+    {
+      type: "confirm",
+      name: "again",
+      message: "Would you like to enter more employees?",
+    },
+  ];
+  const { again, ...answers } = await inquirer.prompt(prompts);
+  const newInputs = [...inputs, answers];
+  return again ? eeQuestions(newInputs) : newInputs;
+};
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
-const teamMembers = [];
-const idArray = [];
+const main = async () => {
+  const inputs = await eeQuestions();
 
-function menu() {
-  function createManager() {
-    console.log("Please Build Your Team!");
-    inquirer
-      .prompt([
-        {
-          name: "managerName",
-          message: "Please enter manager's name: ",
-          validate: validateName,
-        },
-        {
-          name: "managerId",
-          message: "Please enter manager's id: ",
-          validate: validateId,
-        },
-        {
-          name: "managerEmail",
-          message: "Please enter manager's email: ",
-          default: "mail@mail.com",
-          validate: validateEmail,
-        },
-        {
-          type: "list",
-          name: "employeeType",
-          message: "Who would you like to enter first?",
-          choices: ["Manager", "Engineer", "Intern"],
-        },
-        {
-          name: "managerOfficeNumber",
-          message: "Please enter office phone number",
-          when: function (answers) {
-            return answers["employeeType"] === "Manager";
-          },
-        },
-
-        {
-          name: "gitHub",
-          message: "Please enter gitHub account id",
-          when: function (answers) {
-            return answers["employeeType"] === "Engineer";
-          },
-        },
-        {
-          name: "school",
-          message: "Please enter school",
-          when: function (answers) {
-            return answers["employeeType"] === "Intern";
-          },
-        },
-        {
-          type: "confirm",
-          name: "again",
-          message: "Would you like to enter more employees?",
-        },
-      ])
-      .then((answers) => {
-        const manager = new Manager(
-          answers.managerName,
-          answers.managerId,
-          answers.managerEmail,
-          answers.managerOfficeNumber
-        );
-        teamMembers.push(manager);
-        idArray.push(answers.managerId);
-        //createTeam() - this function is going to ask who to create next and then either create intern or engineer
-      });
+  let employee = [];
+  for (let i in inputs) {
+    if (inputs[i].employeeType === "Manager") {
+      let nM = new Manager(
+        inputs[i].employeeName,
+        inputs[i].employeeId,
+        inputs[i].employeeEmail,
+        inputs[i].officeNumber
+      );
+      employee.push(nM);
+    } else if (inputs[i].employeeType === "Engineer") {
+      let nE = new Engineer(
+        inputs[i].employeeName,
+        inputs[i].employeeId,
+        inputs[i].employeeEmail,
+        inputs[i].gitHub
+      );
+      employee.push(nE);
+    } else {
+      let nI = new Intern(
+        inputs[i].employeeName,
+        inputs[i].employeeId,
+        inputs[i].employeeEmail,
+        inputs[i].school
+      );
+      employee.push(nI);
+      const newInputs = [...inputs, answers];
+      return again ? eeQuestions(newInputs) : newInputs;
+    }
   }
 
-  function createTeam() {
-    inquirer
-      .prompt([
-        {}, //ask who would you like to create with choices engineer intern or none
-      ])
-      .then((choice) => {
-        switch (answers.employeeType) {
-          case value:
-            break;
+  fs.writeFile(outputPath, render(employee), (err) => {
+    if (err) {
+      return console.log(err);
+    }
 
-          default:
-            break;
-        }
-      });
-  }
-  function validateId(id) {
-    const reg = /^\d+$/;
-    return reg.test(id) || "ID should be a number!";
-  }
+    console.log("Success!");
+  });
+};
 
-  function validateName(name) {
-    return name !== "" || "Please enter Name";
-  }
-
-  function validateEmail(email) {
-    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return reg.test(email) || "Please enter a valid email";
-  }
-  // function buildTeam() {
-  //   createManager();
-  // }
+function validateId(id) {
+  const reg = /^\d+$/;
+  return reg.test(id) || "ID should be a number!";
 }
-menu();
+
+function validateName(name) {
+  return name !== "" || "Please enter Name";
+}
+
+function validateEmail(email) {
+  const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return reg.test(email) || "Please enter a valid email";
+}
+// function buildTeam() {
+//   createManager();
+// }
+
+main();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
